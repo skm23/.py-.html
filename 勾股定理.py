ea4c3353e675750574bac0fe3f15df42b473ea49
qwert <118148914+skm23@users.noreply.github.com>
2023-04-07 18:24:47 +0800

    Update 勾股定理.py

--- 勾股定理.py
+++ 勾股定理.py
@@ -11,7 +11,7 @@ def share(): #求直角三角形的直角短边
     share = (string ** 2) - (hook ** 2)
     share1 = pow((string ** 2) - (hook ** 2), 0.5)
     if int(share1) == share1:
-        print("share1")
+        print(share1)
     else:
         print(f"√{share}")
 def hook(): #求直角三角形的直角长边
@@ -20,7 +20,7 @@ def hook(): #求直角三角形的直角长边
     hook = (string ** 2) - (share ** 2)
     hook1 = pow((string ** 2) - (share ** 2), 0.5)
     if int(hook1) == hook1:
-        print("hook1")
+        print(hook1)
     else:
         print(f"√{hook}")
 def string(): #求直角三角形的斜边
@@ -29,7 +29,7 @@ def string(): #求直角三角形的斜边
     string = (share ** 2) + (hook ** 2)
     string1 = pow((share ** 2) + (hook ** 2), 0.5)
     if int(string1) == string1:
-        print("string1")
+        print(string1)
     else:
         print(f"√{string}")
 a = int(input())
